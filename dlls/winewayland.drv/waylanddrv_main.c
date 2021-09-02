ad1680ec282cc887382972f27fce72b957fd8f50
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-02 16:22:13 +0300

    winewayland.drv: Introduce private driver window data structure.
    Introduce a private driver window data structure and use it to keep
    track of some basic window data. Access to the information is protected
    for multithreaded access.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv_main.c
+++ dlls/winewayland.drv/waylanddrv_main.c
@@ -30,7 +30,6 @@
 #include "waylanddrv.h"
 
 #include "wine/debug.h"
-#include "wine/gdi_driver.h"
 
 #include <stdlib.h>
 
@@ -84,11 +83,13 @@ static void WAYLAND_ThreadDetach(void)
 static const struct user_driver_funcs waylanddrv_funcs =
 {
     .pCreateWindow = WAYLAND_CreateWindow,
+    .pDestroyWindow = WAYLAND_DestroyWindow,
     .pEnumDisplaySettingsEx = WAYLAND_EnumDisplaySettingsEx,
     .pGetCurrentDisplaySettings = WAYLAND_GetCurrentDisplaySettings,
     .pThreadDetach = WAYLAND_ThreadDetach,
     .pUpdateDisplayDevices = WAYLAND_UpdateDisplayDevices,
     .pWindowMessage = WAYLAND_WindowMessage,
+    .pWindowPosChanging = WAYLAND_WindowPosChanging,
 };
 
 static NTSTATUS waylanddrv_unix_init(void *arg)

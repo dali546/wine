abfd40c96ce06098123ccdb8f5fdd5baffc76646
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-06 13:24:54 +0300

    winewayland.drv: Basic handling of Wayland keyboard events.
    Handle Wayland keyboard events and translate them to Windows events,
    currently using a hardcoded US key mapping.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -27,6 +27,7 @@
 
 #include <stdarg.h>
 #include <wayland-client.h>
+#include <xkbcommon/xkbcommon.h>
 #include "xdg-output-unstable-v1-client-protocol.h"
 #include "xdg-shell-client-protocol.h"
 
@@ -54,6 +55,18 @@ enum wayland_configure_flags
 
 struct wayland_surface;
 struct wayland_shm_buffer;
+
+struct wayland_keyboard
+{
+    struct wl_keyboard *wl_keyboard;
+    struct wayland_surface *focused_surface;
+    int repeat_interval_ms;
+    int repeat_delay_ms;
+    uint32_t pressed_key;
+    uint32_t enter_serial;
+    struct xkb_context *xkb_context;
+    struct xkb_state *xkb_state;
+};
 
 struct wayland_cursor
 {
@@ -90,6 +103,7 @@ struct wayland
     struct zxdg_output_manager_v1 *zxdg_output_manager_v1;
     uint32_t next_fallback_output_id;
     struct wl_list output_list;
+    struct wayland_keyboard keyboard;
     struct wayland_pointer pointer;
     DWORD last_dispatch_mask;
     int event_notification_pipe[2];
@@ -295,6 +309,14 @@ void wayland_window_surface_update_wayland_surface(struct window_surface *surfac
                                                    struct wayland_surface *wayland_surface);
 
 /**********************************************************************
+ *          Wayland Keyboard
+ */
+
+void wayland_keyboard_init(struct wayland_keyboard *keyboard, struct wayland *wayland,
+                           struct wl_keyboard *wl_keyboard);
+void wayland_keyboard_deinit(struct wayland_keyboard *keyboard);
+
+/**********************************************************************
  *          Wayland Pointer/Cursor
  */
 
@@ -304,5 +326,11 @@ void wayland_pointer_deinit(struct wayland_pointer *pointer);
 void wayland_cursor_destroy(struct wayland_cursor *wayland_cursor);
 BOOL wayland_init_set_cursor(void);
 void wayland_invalidate_set_cursor(void);
+
+/**********************************************************************
+ *          XKB helpers
+ */
+xkb_layout_index_t _xkb_state_get_active_layout(struct xkb_state *xkb_state);
+int _xkb_keysyms_to_utf8(const xkb_keysym_t *syms, int nsyms, char *utf8, int utf8_size);
 
 #endif /* __WINE_WAYLANDDRV_H */

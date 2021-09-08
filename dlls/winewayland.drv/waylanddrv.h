1a5886b18a356b0543334c6cc9c77650feffe69d
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-08 11:34:08 +0300

    winewayland.drv: Refine heuristics for relative positioning of windows.
    Before this commit, the simple heuristic for using a Wayland subsurface
    and relative positioning to display a window was: it doesn't have a
    caption AND it has an owner.
    
    Since many applications use unowned top-level windows for various kinds
    of popups, this commit enhances our heuristic to detect such windows and
    use relative positioning for them too. Since for such windows we don't
    have a owner window, and thus a Wayland surface parent to attach them
    to, we use the most recent surface the user interacted with as the
    effective owner.
    
    The updated heuristic is: If a window has an effective owner AND it's
    not a child window AND it's not minimized AND it doesn't have a caption,
    min/max buttons or sysmenu AND it's relatively small compared to the
    monitor size THEN we use subsurface/relative positioning.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -150,6 +150,7 @@ struct wayland
     struct wayland_keyboard keyboard;
     struct wayland_pointer pointer;
     DWORD last_dispatch_mask;
+    int last_event_type;
     int event_notification_pipe[2];
 };
 
@@ -471,6 +472,13 @@ static inline BOOL intersect_rect(RECT *dst, const RECT *src1, const RECT *src2)
     dst->right = min(src1->right, src2->right);
     dst->bottom = min(src1->bottom, src2->bottom);
     return !IsRectEmpty(dst);
+}
+
+static inline HWND get_focus(void)
+{
+    GUITHREADINFO info;
+    info.cbSize = sizeof(info);
+    return NtUserGetGUIThreadInfo(GetCurrentThreadId(), &info) ? info.hwndFocus : 0;
 }
 
 /**********************************************************************

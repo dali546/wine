488c5a0349503d3974af734d2ee58ff6e297afc8
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-08 15:53:46 +0300

    winewayland.drv: Support window and window_surface regions.
    Use buffers with an alpha channel (ARGB8888) to implement window
    regions. Pixels outside the region are left as transparent in the
    buffer.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -332,6 +332,8 @@ void CDECL wayland_window_surface_flush(struct window_surface *window_surface);
 BOOL wayland_window_surface_needs_flush(struct window_surface *surface);
 void wayland_window_surface_update_wayland_surface(struct window_surface *surface,
                                                    struct wayland_surface *wayland_surface);
+void wayland_window_surface_set_window_region(struct window_surface *window_surface,
+                                              HRGN win_region);
 
 /**********************************************************************
  *          Wayland Keyboard

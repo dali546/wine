4cf3165af57acf2978e9b061d087534f82b78da1
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-10 15:38:48 +0300

    winewayland.drv: Store a list of Wayland child surfaces.
    Make each Wayland surfaces aware of its subsurfaces, and also maintain a
    list of toplevel surfaces. This will allow us to efficiently apply
    property changes (e.g., changing the output associated with a surface)
    to whole surface tree hierarchies in upcoming commits.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -126,6 +126,7 @@ struct wayland
     struct zxdg_output_manager_v1 *zxdg_output_manager_v1;
     uint32_t next_fallback_output_id;
     struct wl_list output_list;
+    struct wl_list toplevel_list;
     struct wayland_keyboard keyboard;
     struct wayland_pointer pointer;
     DWORD last_dispatch_mask;
@@ -173,6 +174,7 @@ struct wayland_surface_configure
 
 struct wayland_surface
 {
+    struct wl_list link;
     struct wayland *wayland;
     struct wl_surface *wl_surface;
     struct wl_subsurface *wl_subsurface;
@@ -187,6 +189,7 @@ struct wayland_surface
     LONG ref;
     enum wayland_surface_role role;
     BOOL drawing_allowed;
+    struct wl_list child_list;
 };
 
 struct wayland_buffer_queue

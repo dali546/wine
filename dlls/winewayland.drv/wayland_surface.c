c1af06953009debe50df8549fa4ef0ea9395b8e4
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-11 01:51:44 +0300

    winewayland.drv: Use output scale on Wayland surface buffers.
    Use the scale of the main output of each Wayland surface to always
    render at the native resolution on that output. This effectively turns
    off compositor-side scaling, giving applications/Wine complete control
    over how to scale.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/wayland_surface.c
+++ dlls/winewayland.drv/wayland_surface.c
@@ -345,6 +345,8 @@ struct wayland_surface *wayland_surface_create_subsurface(struct wayland *waylan
 
     surface->main_output = parent->main_output;
     surface->wine_output = parent->wine_output;
+    wl_surface_set_buffer_scale(surface->wl_surface,
+                                wayland_surface_get_buffer_scale(parent));
 
     wl_surface_commit(surface->wl_surface);
 
@@ -621,6 +623,17 @@ void wayland_surface_unmap(struct wayland_surface *surface)
     LeaveCriticalSection(&surface->crit);
 }
 
+/* If the surface has a main output set, use that, otherwise use the output
+ * which Wine considers to contain the associated window. */
+static struct wayland_output *wayland_surface_get_main_output(
+        struct wayland_surface *surface)
+{
+    if (surface->main_output)
+        return surface->main_output;
+
+    return surface->wine_output;
+}
+
 /**********************************************************************
  *          wayland_surface_coords_to_screen
  *
@@ -655,8 +668,10 @@ void wayland_surface_coords_from_wine(struct wayland_surface *surface,
                                       int wine_x, int wine_y,
                                       double *wayland_x, double *wayland_y)
 {
-    *wayland_x = wine_x;
-    *wayland_y = wine_y;
+    int scale = wayland_surface_get_buffer_scale(surface);
+
+    *wayland_x = wine_x / (double)scale;
+    *wayland_y = wine_y / (double)scale;
 }
 
 /**********************************************************************
@@ -684,8 +699,10 @@ void wayland_surface_coords_to_wine(struct wayland_surface *surface,
                                     double wayland_x, double wayland_y,
                                     int *wine_x, int *wine_y)
 {
-    *wine_x = wayland_x;
-    *wine_y = wayland_y;
+    int scale = wayland_surface_get_buffer_scale(surface);
+
+    *wine_x = wayland_x * scale;
+    *wine_y = wayland_y * scale;
 }
 
 /**********************************************************************
@@ -715,17 +732,19 @@ void wayland_surface_unref(struct wayland_surface *surface)
         wayland_surface_destroy(surface);
 }
 
-static void wayland_surface_tree_set_main_output(struct wayland_surface *surface,
-                                                 struct wayland_output *output)
+static void wayland_surface_tree_set_main_output_and_scale(struct wayland_surface *surface,
+                                                           struct wayland_output *output,
+                                                           int scale)
 {
     struct wayland_surface *child;
 
     surface->main_output = output;
+    wl_surface_set_buffer_scale(surface->wl_surface, scale);
 
     EnterCriticalSection(&surface->crit);
 
     wl_list_for_each(child, &surface->child_list, link)
-        wayland_surface_tree_set_main_output(child, output);
+        wayland_surface_tree_set_main_output_and_scale(child, output, scale);
 
     LeaveCriticalSection(&surface->crit);
 }
@@ -749,7 +768,8 @@ static void wayland_surface_set_main_output(struct wayland_surface *surface,
 
     if (surface->main_output != output)
     {
-        wayland_surface_tree_set_main_output(surface, output);
+        wayland_surface_tree_set_main_output_and_scale(surface, output,
+                                                       output ? output->scale : 1);
         if (surface->hwnd)
             SendMessageW(surface->hwnd, WM_WAYLAND_SURFACE_OUTPUT_CHANGE, 0, 0);
     }
@@ -822,6 +842,26 @@ void wayland_surface_set_wine_output(struct wayland_surface *surface,
 
     if (surface->wine_output != output)
         wayland_surface_tree_set_wine_output(surface, output);
+}
+
+/**********************************************************************
+ *          wayland_surface_get_buffer_scale
+ *
+ */
+int wayland_surface_get_buffer_scale(struct wayland_surface *surface)
+{
+    /* Use the toplevel surface to get the scale */
+    struct wayland_surface *toplevel = surface;
+    struct wayland_output *output;
+    int scale = 1;
+
+    while (toplevel->parent) toplevel = toplevel->parent;
+
+    output = wayland_surface_get_main_output(surface);
+    if (output) scale = output->scale;
+
+    TRACE("hwnd=%p (toplevel=%p) => scale=%d\n", surface->hwnd, toplevel->hwnd, scale);
+    return scale;
 }
 
 /**********************************************************************

39602dcb35c847a5aa27efedc83fb0d18fb17c5c
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-10 18:40:24 +0300

    winewayland.drv: Handle Wayland surface output positioning.
    Track the output(s) a Wayland surface is visible in, to allow us to
    update the position of the associated window in the Windows virtual
    screen space.
    
    We always place windows at the top-left origin of their main output.
    The main output is updated to be the leftmost output a surface has
    entered. Support for more intuitive handling of windows with RTL layouts
    remains to be done.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -47,6 +47,7 @@ enum wayland_window_message
     WM_WAYLAND_SET_CURSOR = 0x80001000,
     WM_WAYLAND_CONFIGURE = 0x80001001,
     WM_WAYLAND_STATE_UPDATE = 0x80001002,
+    WM_WAYLAND_SURFACE_OUTPUT_CHANGE = 0x80001003
 };
 
 enum wayland_configure_flags
@@ -160,6 +161,12 @@ struct wayland_surface_configure
     BOOL processed;
 };
 
+struct wayland_output_ref
+{
+    struct wl_list link;
+    struct wayland_output *output;
+};
+
 struct wayland_surface
 {
     struct wl_list link;
@@ -175,6 +182,8 @@ struct wayland_surface
     struct wayland_surface_configure current;
     BOOL mapped;
     LONG ref;
+    struct wl_list output_ref_list;
+    struct wayland_output *main_output;
     BOOL drawing_allowed;
     struct wl_list child_list;
 };
@@ -305,6 +314,8 @@ void wayland_surface_coords_to_wine(struct wayland_surface *surface,
                                     int *wine_x, int *wine_y);
 struct wayland_surface *wayland_surface_ref(struct wayland_surface *surface);
 void wayland_surface_unref(struct wayland_surface *surface);
+void wayland_surface_leave_output(struct wayland_surface *surface,
+                                  struct wayland_output *output);
 void wayland_surface_set_drawing_allowed(struct wayland_surface *surface, BOOL allowed);
 BOOL wayland_surface_is_drawing_allowed(struct wayland_surface *surface);
 

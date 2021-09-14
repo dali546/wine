9e8c1e6f89306304e3f072cdb73317964f539773
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-14 13:19:52 +0300

    winewayland.drv: Add infrastructure to associate an EGL surface with a window.
    Introduce the internal wayland_gl_drawable object, which associates a
    window (and its backing Wayland surface) with an EGL surface.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/window.c
+++ dlls/winewayland.drv/window.c
@@ -179,7 +179,7 @@ static struct wayland_win_data *wayland_win_data_create(HWND hwnd)
  *
  *  Gets the wayland surface for HWND while locking the private window data.
  */
-static struct wayland_surface *wayland_surface_for_hwnd_lock(HWND hwnd)
+struct wayland_surface *wayland_surface_for_hwnd_lock(HWND hwnd)
 {
     struct wayland_win_data *data = wayland_win_data_get(hwnd);
 
@@ -194,7 +194,7 @@ static struct wayland_surface *wayland_surface_for_hwnd_lock(HWND hwnd)
 /***********************************************************************
  *           wayland_surface_for_hwnd_unlock
  */
-static void wayland_surface_for_hwnd_unlock(struct wayland_surface *surface)
+void wayland_surface_for_hwnd_unlock(struct wayland_surface *surface)
 {
     if (surface) LeaveCriticalSection(&win_data_section);
 }

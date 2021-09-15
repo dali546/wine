22b0f33f33d4ce56721aea8437407aaabc596beb
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-15 15:40:50 +0300

    winewayland.drv: Support setting Wayland pointer to relative event mode.
    Introduce a function to set the Wayland pointer event mode, emitting
    either absolute coordinates (the default) or relative coordinates (i.e.,
    just differences from previous positions). Relative mode requires and
    uses the zwp_relative_pointer_manager_v1 Wayland extension.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -30,6 +30,7 @@
 #include <wayland-egl.h>
 #include <xkbcommon/xkbcommon.h>
 #include <xkbcommon/xkbcommon-compose.h>
+#include "relative-pointer-unstable-v1-client-protocol.h"
 #include "viewporter-client-protocol.h"
 #include "xdg-output-unstable-v1-client-protocol.h"
 #include "xdg-shell-client-protocol.h"
@@ -97,6 +98,7 @@ struct wayland_pointer
     uint32_t enter_serial;
     struct wayland_cursor *cursor;
     HCURSOR hcursor;
+    struct zwp_relative_pointer_v1 *zwp_relative_pointer_v1;
 };
 
 struct wayland
@@ -114,6 +116,7 @@ struct wayland
     struct wl_shm *wl_shm;
     struct wl_seat *wl_seat;
     struct wp_viewporter *wp_viewporter;
+    struct zwp_relative_pointer_manager_v1 *zwp_relative_pointer_manager_v1;
     struct zxdg_output_manager_v1 *zxdg_output_manager_v1;
     uint32_t next_fallback_output_id;
     struct wl_list output_list;
@@ -399,6 +402,7 @@ void wayland_keyboard_update_layout(struct wayland_keyboard *keyboard);
 void wayland_pointer_init(struct wayland_pointer *pointer, struct wayland *wayland,
                           struct wl_pointer *wl_pointer);
 void wayland_pointer_deinit(struct wayland_pointer *pointer);
+void wayland_pointer_set_relative(struct wayland_pointer *pointer, BOOL relative);
 void wayland_cursor_destroy(struct wayland_cursor *wayland_cursor);
 BOOL wayland_init_set_cursor(void);
 void wayland_invalidate_set_cursor(void);

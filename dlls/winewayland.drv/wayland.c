49eaf25cb3142fce210d5602f9b2c169a228f0d3
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-15 15:40:50 +0300

    winewayland.drv: Support setting Wayland pointer to relative event mode.
    Introduce a function to set the Wayland pointer event mode, emitting
    either absolute coordinates (the default) or relative coordinates (i.e.,
    just differences from previous positions). Relative mode requires and
    uses the zwp_relative_pointer_manager_v1 Wayland extension.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/wayland.c
+++ dlls/winewayland.drv/wayland.c
@@ -172,6 +172,11 @@ static void registry_handle_global(void *data, struct wl_registry *registry,
     {
         wayland->wp_viewporter = wl_registry_bind(registry, id, &wp_viewporter_interface, 1);
     }
+    else if (strcmp(interface, "zwp_relative_pointer_manager_v1") == 0)
+    {
+        wayland->zwp_relative_pointer_manager_v1 =
+            wl_registry_bind(registry, id, &zwp_relative_pointer_manager_v1_interface, 1);
+    }
 }
 
 static void registry_handle_global_remove(void *data, struct wl_registry *registry,
@@ -327,6 +332,9 @@ void wayland_deinit(struct wayland *wayland)
 
     if (wayland->wl_seat)
         wl_seat_destroy(wayland->wl_seat);
+
+    if (wayland->zwp_relative_pointer_manager_v1)
+        zwp_relative_pointer_manager_v1_destroy(wayland->zwp_relative_pointer_manager_v1);
 
     if (wayland->wp_viewporter)
         wp_viewporter_destroy(wayland->wp_viewporter);

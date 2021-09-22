59ca9513992c435cc60d29a47ab9d84a9e2ab356
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-22 17:30:21 +0300

    winewayland.drv: Implement IDataObject::QueryGetData for data offers.
    Check if for any of the available mime types of the associated data
    offer we support converting from that mime type to the requested Windows
    clipboard format.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv_main.c
+++ dlls/winewayland.drv/waylanddrv_main.c
@@ -225,6 +225,7 @@ const unixlib_entry_t __wine_unix_call_funcs[] =
     waylanddrv_unix_init,
     waylanddrv_unix_read_events,
     waylanddrv_unix_clipboard_message,
+    waylanddrv_unix_data_offer_accept_format,
 };
 
 C_ASSERT(ARRAYSIZE(__wine_unix_call_funcs) == waylanddrv_unix_func_count);

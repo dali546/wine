35ab89b63856422d478b6e8597062d7df201f473
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-23 13:02:14 +0300

    winewayland.drv: Handle the wl_data_device::leave event.
    Relay the event to the IDropTarget at the current drag-and-drop
    position.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/unixlib.h
+++ dlls/winewayland.drv/unixlib.h
@@ -89,6 +89,7 @@ C_ASSERT(waylanddrv_client_func_last <= NtUserDriverCallbackLast + 1);
 enum waylanddrv_client_dnd_event
 {
     CLIENT_DND_EVENT_ENTER,
+    CLIENT_DND_EVENT_LEAVE,
 };
 
 struct waylanddrv_client_dnd_params

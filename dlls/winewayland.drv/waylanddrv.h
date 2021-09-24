aedd07c66440a23cabb4aeb7b328efe8b6982912
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-24 13:36:40 +0300

    winewayland.drv: Add debugging helpers for pixel buffer contents.
    During development it's often useful to be able inspect the contents and
    window or damage regions of the various buffers involved in software
    rendering.
    
    This commit adds helper functions to dump buffer contents to disk, and
    uses them to dump the window buffer at flush time, the wayland SHM
    buffers at commit time, and the front buffer at update/glFlush time.
    
    These helpers are turned off by default and can be enabled by changing
    the value of the corresponding preprocessor define.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -760,6 +760,8 @@ size_t ascii_to_unicode_z(WCHAR *dst, size_t dst_max_chars,
 int wayland_shmfd_create(const char *name, int size) DECLSPEC_HIDDEN;
 void wayland_get_client_rect_in_screen_coords(HWND hwnd, RECT *client_rect) DECLSPEC_HIDDEN;
 void wayland_get_client_rect_in_win_top_left_coords(HWND hwnd, RECT *client_rect) DECLSPEC_HIDDEN;
+void dump_pixels(const char *fpattern, int dbgid, unsigned int *pixels, int width, int height,
+                 BOOL alpha, HRGN damage, HRGN win_region) DECLSPEC_HIDDEN;
 
 /**********************************************************************
  *          USER32 helpers

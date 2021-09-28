85f6d5eec023c4241a8a16709279e222aa283870
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-28 13:24:03 +0300

    winewayland.drv: Add function to unmap a Wayland surface.
    Keep track of whether a Wayland surface is mapped or not, and allow
    unmapping (i.e., hiding) it.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/wayland_surface.c
+++ dlls/winewayland.drv/wayland_surface.c
@@ -354,6 +354,7 @@ void wayland_surface_commit_buffer(struct wayland_surface *surface,
     }
 
     wl_surface_commit(surface->wl_surface);
+    surface->mapped = TRUE;
 
     LeaveCriticalSection(&surface->crit);
 
@@ -405,6 +406,22 @@ void wayland_surface_destroy(struct wayland_surface *surface)
     wl_display_flush(surface->wayland->wl_display);
 
     heap_free(surface);
+}
+
+/**********************************************************************
+ *          wayland_surface_unmap
+ *
+ * Unmaps (i.e., hides) this surface.
+ */
+void wayland_surface_unmap(struct wayland_surface *surface)
+{
+    EnterCriticalSection(&surface->crit);
+
+    wl_surface_attach(surface->wl_surface, NULL, 0, 0);
+    wl_surface_commit(surface->wl_surface);
+    surface->mapped = FALSE;
+
+    LeaveCriticalSection(&surface->crit);
 }
 
 /**********************************************************************

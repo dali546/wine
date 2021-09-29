58dd8c9b1d7b85a618f037e9a1fde5251176e75d
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-29 13:47:29 +0300

    winewayland.drv: Implement ChangeDisplaySettingsEx.
    Change the current Wine display mode the and notify all Wayland threads
    about the change.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv_main.c
+++ dlls/winewayland.drv/waylanddrv_main.c
@@ -120,6 +120,7 @@ static void CDECL WAYLAND_ThreadDetach(void)
 
 static const struct user_driver_funcs waylanddrv_funcs =
 {
+    .pChangeDisplaySettingsEx = WAYLAND_ChangeDisplaySettingsEx,
     .pCreateWindow = WAYLAND_CreateWindow,
     .pDestroyWindow = WAYLAND_DestroyWindow,
     .pEnumDisplaySettingsEx = WAYLAND_EnumDisplaySettingsEx,

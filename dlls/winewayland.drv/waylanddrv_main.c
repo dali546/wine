25f192f1c477145cad99b80d6655c9ac3c8357a2
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-29 14:17:56 +0300

    winewayland.drv: Add skeleton OpenGL driver.
    The driver opens the EGL and GL libraries and creates stubs for standard
    GL functions.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv_main.c
+++ dlls/winewayland.drv/waylanddrv_main.c
@@ -403,6 +403,7 @@ static const struct user_driver_funcs waylanddrv_funcs =
     .pWindowMessage = WAYLAND_WindowMessage,
     .pWindowPosChanged = WAYLAND_WindowPosChanged,
     .pWindowPosChanging = WAYLAND_WindowPosChanging,
+    .pwine_get_wgl_driver = WAYLAND_wine_get_wgl_driver,
 };
 
 static void wayland_init_process_name(void)

81fd5ae9723689387302e4300b8fa50118784f31
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-09-30 11:24:36 +0300

    winewayland.drv: Destroy GL drawable on window destruction.
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/window.c
+++ dlls/winewayland.drv/window.c
@@ -657,6 +657,7 @@ void CDECL WAYLAND_DestroyWindow(HWND hwnd)
     TRACE("%p\n", hwnd);
 
     if (!(data = wayland_win_data_get(hwnd))) return;
+    wayland_destroy_gl_drawable(hwnd);
     wayland_win_data_destroy(data);
 }
 

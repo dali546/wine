56ae860ad6de64e052998cfbc8a926e3ca6c70a3
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2021-11-08 13:23:34 +0200

    winewayland.drv: Track the Wine monitor associated with a Wayland output.
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -102,6 +102,7 @@ struct wayland_output
     int x, y;  /* position in native pixel coordinate space */
     int scale; /* wayland output scale factor for hidpi */
     char *name;
+    WCHAR wine_name[128];
     uint32_t global_id;
 };
 
@@ -167,6 +168,9 @@ BOOL wayland_output_create(struct wayland *wayland, uint32_t id, uint32_t versio
 void wayland_output_destroy(struct wayland_output *output);
 void wayland_output_use_xdg_extension(struct wayland_output *output);
 void wayland_notify_wine_monitor_change(void);
+void wayland_update_outputs_from_process(struct wayland *wayland);
+struct wayland_output *wayland_output_get_by_wine_name(struct wayland *wayland,
+                                                       LPCWSTR wine_name);
 
 /**********************************************************************
  *          USER driver functions

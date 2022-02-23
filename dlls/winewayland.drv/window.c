c0bc1b6bc89d1938534e3103dd8a8eccbd2822f7
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2022-02-23 12:28:33 +0200

    winewayland.drv: Implement Wine monitor change notification.
    Introduce a mechanism to synchronously notify all Wayland capable
    threads about changes in the current Wine monitor setup.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- dlls/winewayland.drv/window.c
+++ dlls/winewayland.drv/window.c
@@ -52,6 +52,10 @@ BOOL WAYLAND_CreateWindow(HWND hwnd)
     return TRUE;
 }
 
+static void handle_wm_wayland_monitor_change(struct wayland *wayland)
+{
+}
+
 /**********************************************************************
  *           WAYLAND_WindowMessage
  */
@@ -69,6 +73,9 @@ LRESULT WAYLAND_WindowMessage(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
                                virtual_rect.bottom - virtual_rect.top,
                                SWP_NOZORDER | SWP_NOACTIVATE | SWP_DEFERERASE);
         }
+        break;
+    case WM_WAYLAND_MONITOR_CHANGE:
+        handle_wm_wayland_monitor_change(thread_wayland());
         break;
     default:
         FIXME("got window msg %x hwnd %p wp %lx lp %lx\n", msg, hwnd, wp, lp);

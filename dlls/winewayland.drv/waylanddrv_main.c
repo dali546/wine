29c75f759f64192df638626f56fdfb68a7170461
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2022-06-07 12:17:12 +0300

    winewayland.drv: Add initial unixlib stub.
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

---
+++ dlls/winewayland.drv/waylanddrv_main.c
@@ -0,0 +1,52 @@
+/*
+ * WAYLANDDRV initialization code
+ *
+ * Copyright 2020 Alexandre Frantzis for Collabora Ltd
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#if 0
+#pragma makedep unix
+#endif
+
+#include "config.h"
+
+#include "ntstatus.h"
+#define WIN32_NO_STATUS
+
+#include "waylanddrv.h"
+
+static NTSTATUS CDECL waylanddrv_unix_call(enum waylanddrv_unix_func func, void *params);
+
+static NTSTATUS waylanddrv_unix_init(void *arg)
+{
+    struct waylanddrv_unix_init_params *params = arg;
+    params->unix_call = waylanddrv_unix_call;
+    return 0;
+}
+
+const unixlib_entry_t __wine_unix_call_funcs[] =
+{
+    waylanddrv_unix_init,
+};
+
+C_ASSERT(ARRAYSIZE(__wine_unix_call_funcs) == waylanddrv_unix_func_count);
+
+/* FIXME: Use __wine_unix_call instead */
+static NTSTATUS CDECL waylanddrv_unix_call(enum waylanddrv_unix_func func, void *params)
+{
+    return __wine_unix_call_funcs[func](params);
+}
